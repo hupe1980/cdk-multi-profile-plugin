e943a53e1b5ae54158760169ca4da648464d9cd8
hupe1980 <frankhuebner1980@gmail.com>
2019-10-30 10:51:16 +0100

    Initial commit

--- lib/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const tokenCodeFn: (mfaSerial: string, callback: (err?: Error | undefined, token?: string | undefined) => void) => Promise<void>;
-export declare const readProfiles: () => {
-    [key: string]: string;
-};
-export declare const getSharedCredentialsFilename: () => string;
