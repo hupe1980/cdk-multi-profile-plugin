b02e4b1ef3b73f6f2820c12c7b81aafbfa4c61f5
André von Deetzen <andre@vondeetzen.de>
2019-12-25 11:00:22 +0100

    Ensure data set for integration test of precedence mapping

--- cdk-multi-profile-plugin/src/__tests__/precedence-mapper.test.ts
+++ cdk-multi-profile-plugin/src/__tests__/precedence-mapper.test.ts
@@ -1,5 +1,83 @@
 import {readProfiles} from "../utils";
-import {EnvironmentAwareGlobalProfileMapper} from "../profile-mapper";
+import {EnvironmentAwareGlobalProfileMapper, JsonFileProfileMapper} from "../profile-mapper";
+
+// These tests sets ensure the necessary data sets for the following tests.
+describe('Ensure testset', () => {
+    describe('Profiles that must be configured in package.json', () => {
+        let profiles: { [key: string]: string };
+        beforeEach(() => {
+            profiles = new JsonFileProfileMapper({
+                workingDirectory: process.cwd(),
+                filename: 'package.json'
+            }).resolve();
+        });
+
+        it('should contain a mapping for account 123', () => {
+            expect(profiles['123']).toBe('default123');
+        });
+
+        it('should contain a mapping for account 456', () => {
+            expect(profiles['456']).toBe('default123');
+        });
+
+        it('should contain a mapping for account 435', () => {
+            expect(profiles['435']).toBe('default435');
+        });
+
+        it('should contain a mapping for account 765', () => {
+            expect(profiles['765']).toBe('default765');
+        });
+    });
+
+    describe('Profiles that must be configured in cdkmultiprofileplugin.json', () => {
+        let profiles: { [key: string]: string };
+        beforeEach(() => {
+            profiles = new JsonFileProfileMapper({
+                workingDirectory: process.cwd(),
+                filename: 'cdkmultiprofileplugin.json'
+            }).resolve();
+        });
+
+        it('should contain a mapping for account 678', () => {
+            expect(profiles['678']).toBe('project678');
+        });
+
+        it('should contain a mapping for account 345', () => {
+            expect(profiles['345']).toBe('project345');
+        });
+
+        it('should contain a mapping for account 435', () => {
+            expect(profiles['435']).toBe('project435');
+        });
+
+        it('should contain a mapping for account 765', () => {
+            expect(profiles['765']).toBe('project765');
+        });
+    });
+
+    describe('Profiles that must be configured in global configuration', () => {
+        let profiles: { [key: string]: string };
+        beforeEach(() => {
+            profiles = new JsonFileProfileMapper({
+                workingDirectory: `${process.cwd()}/src/__tests__/`,
+                filename: 'global.plugin.config.json'
+            }).resolve();
+        });
+
+        it('should contain a mapping for account 345', () => {
+            expect(profiles['345']).toBe('global345');
+        });
+
+        it('should contain a mapping for account 765', () => {
+            expect(profiles['765']).toBe('global765');
+        });
+
+        it('should contain a mapping for account 980', () => {
+            expect(profiles['980']).toBe('global980');
+        });
+
+    });
+});
 
 describe('Should resolve profile name from package.json', () => {
     let profiles: { [key: string]: string };
@@ -54,5 +132,9 @@ describe('Global configuration', () => {
 
     it('should be undefined if account is not mapped in global, project or package.json scope', () => {
         expect(profiles['999']).toBeUndefined();
+    });
+
+    it('should override a mapping configured only in package.json', () => {
+        expect(profiles['345']).toBe('global345')
     });
 });
