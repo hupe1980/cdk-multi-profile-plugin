6259ff3baa268d6b12e42bde98c16d8721c030f0
Peer Overbeck <peer.overbeck@ewe.de>
2022-04-29 13:26:37 +0200

    fix aws core reference

--- examples/multi-profile-example/lib/example-stack.ts
+++ examples/multi-profile-example/lib/example-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from 'aws-cdk-lib/core';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class ExampleStack extends Stack {
